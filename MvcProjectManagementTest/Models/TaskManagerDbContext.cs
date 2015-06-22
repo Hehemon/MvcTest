b56f519d8b1f872653c4a5164a409b185ef7d9db
Hehemon <hehemon@gmail.com>
2015-06-22 18:23:22 +0300

    Added DbContext

---
+++ MvcProjectManagementTest/Models/TaskManagerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcProjectManagementTest.Models
+{
+    public class TaskManagerDbContext : DbContext
+    {
+        public DbSet<Performer> Performers { get; set; }
+        public DbSet<ProductTask> Tasks { get; set; }
+    }
+}
