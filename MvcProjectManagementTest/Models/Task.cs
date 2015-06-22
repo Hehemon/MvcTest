6f7df73ad08222c5bb95d9430574d333298cdbc7
Hehemon <hehemon@gmail.com>
2015-06-22 17:20:40 +0300

    Added: basic models

---
+++ MvcProjectManagementTest/Models/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcProjectManagementTest.Models
+{
+    public enum TaskStatus
+    {
+        NotStarted,
+        InProcess,
+        Done,
+        Delayed,
+    }
+
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TimeSpan PlannedTime { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TaskStatus Status { get; set; }
+        
+    }
+}
