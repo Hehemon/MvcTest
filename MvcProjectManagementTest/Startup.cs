c01a3595a90d5974f7f29c7b9b0a91f07289c16c
Hehemon <hehemon@gmail.com>
2015-06-22 17:08:09 +0300

    Initial Project

---
+++ MvcProjectManagementTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcProjectManagementTest.Startup))]
+namespace MvcProjectManagementTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
